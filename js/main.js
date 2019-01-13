c546c62d19e6f4ed2f7c06369e8e5fe7b82682ff
Yarbek <yarbek19951@gmail.com>
2019-01-13 16:11:51 +0500

    nav bar qushdim

--- js/main.js
+++ js/main.js
@@ -1,10 +1,31 @@
 $(document).ready(function(){
 
+    $(window).scroll(function(e) {
+      var newScroll = e.currentTarget.scrollY;
+      console.log(e.currentTarget.scrollY);
+      if(newScroll>15){
+        $("#nav").css("top","0");
+        $("#nav").css("width","100%");
+        $("#nav").css ("position","fixed");
+      }else{
+        $("#nav").css("position","");
+      }
+
+
+    });
+
+
+    $('.works-about').slick({
+      lazyLoad: 'ondemand',
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      dots:true,
+      autoplay: true,
+      autoplaySpeed: 5000,
+    });
     $("#biograp").click(function(){
-      $(".education").css("display","none");
-      $(".works").css("display","none");
-      $(".expersion").css("display","none");
-      $(".biograp").toggle("slow");
+      $(".container-about > div").css("display","none");
+      $(".biograp").show();
 
       $("#education").attr("class","");
       $("#works").attr("class","");
@@ -12,10 +33,8 @@ $(document).ready(function(){
       $("#biograp").attr("class","active");
     })
     $("#education").click(function(){
-      $(".biograp").css("display","none");
-      $(".works").css("display","none");
-      $(".expersion").css("display","none");
-      $(".education").toggle("slow");
+      $(".container-about > div").css("display","none");
+      $(".education").show();
 
       $("#biograp").attr("class","");
       $("#works").attr("class","");
@@ -25,10 +44,8 @@ $(document).ready(function(){
 
     })
     $("#works").click(function(){
-      $(".biograp").css("display","none");
-      $(".education").css("display","none");
-      $(".expersion").css("display","none");
-      $(".works").toggle("slow");
+      $(".container-about > div").css("display","none");
+      $(".works").show();
 
       $("#biograp").attr("class","");
       $("#expersion").attr("class","");
@@ -36,10 +53,8 @@ $(document).ready(function(){
       $("#works").attr("class","active");
     })
     $("#expersion").click(function(){
-      $(".biograp").css("display","none");
-      $(".works").css("display","none");
-      $(".education").css("display","none");
-      $(".expersion").toggle("slow");
+      $(".container-about > div").css("display","none");
+      $(".expersion").show();
 
       $("#biograp").attr("class","");
       $("#works").attr("class","");
